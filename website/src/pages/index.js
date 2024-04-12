8fe68c756eed81be40c70549b245886d22ed6ca2
Ulrich Kautz <ulrichkautz@meta.com>
2024-04-12 02:11:09 -0700

    A logo, much joy
    Summary:
    This CL adds the beautiful logo we have to the site documentation website.
    
    # What?
    
    Many changes:
    - Homepage, where now the Scrut logo proudly shines with a new color theme
    - Every documentation page, where links are in the new theme
    - Everywhere the small logo and new favicon are shown
    
    # Why?
    
    It looks great.
    
    Reviewed By: abesto
    
    Differential Revision: D56023292
    
    fbshipit-source-id: e9814d5fbfd8ec8b34bffcf30c2647d3b67771df

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -5,60 +5,61 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
-import {FbInternalOnly, OssOnly} from 'docusaurus-plugin-internaldocs-fb/internal';
-
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import HomepageFeatures from "../components/HomepageFeatures";
+import {
+  FbInternalOnly,
+  OssOnly,
+} from "docusaurus-plugin-internaldocs-fb/internal";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <FbInternalOnly>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs/fb/using-scrut-with-buck/">
-              Tutorial - 10min ⏱️
-            </Link>
-          </div>
+    <header
+      className={clsx("hero hero--primary hero--image", styles.heroImage)}
+    >
+      <h1 className="hero__title">{siteConfig.title}</h1>
+      <h2 className="hero__subtitle">{siteConfig.tagline}</h2>
+    </header>
+  );
+}
+
+function HomepageTutorial() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h2>Start Here:</h2>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/tutorial"
+          >
+            Tutorial - 10min ⏱️
+          </Link>
         </div>
-      </FbInternalOnly>
-      <OssOnly>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs/tutorial">
-              Tutorial - 10min ⏱️
-            </Link>
-          </div>
-        </div>
-      </OssOnly>
+      </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
+      <HomepageTutorial />
     </Layout>
   );
 }
